e01d2a824adb839116efca79753270f89592face
LAPTOP-IGC9RG99\user <dtphu0169@gmail.com>
2025-01-18 21:45:20 +0700

    add singleMovie + custome hook fetch api

--- src/context.js
+++ src/context.js
@@ -1,47 +1,12 @@
 import React , { useContext, useEffect, useState } from "react";
-
-const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+import useFetch from "./useFetch";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
 
-    const [isLoading, setisLoading] = useState(true);
-    const [movie, setMovie] = useState([]);
-    const [isError, setIsError] = useState({show: false, msg: ""});
-    const [query, setQuery] = useState('titanic');    
-
-    const getMovies = async(url) => {
-        setisLoading(true);
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            console.log("data", data);
-            if (data.Response === "True") {
-                setisLoading(false);
-                setIsError({
-                    show: false,
-                    msg: "",
-                })
-                setMovie(data.Search);
-            } else {
-                setIsError({
-                    show: true,
-                    msg: data.Error,
-                })
-            }
-        } catch (error) {
-            console.log("getData error ",error);
-        }
-    }
-
-    useEffect(() => {
-        let timeOut = setTimeout(() => {
-            getMovies(`${API_URL}&s=${query}`);
-        }, 800);
-
-        return () => clearTimeout(timeOut);
-    }, [query]);
+    const [query, setQuery] = useState('titanic');
+    const { isLoading, movie, isError } = useFetch(`&s=${query}`);
 
     return (
         <AppContext.Provider value={{ isLoading, isError, movie, query, setQuery}}> {children} </AppContext.Provider>
